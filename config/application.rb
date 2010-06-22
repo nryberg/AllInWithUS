bea60a905790454a026f23a24532ab34bb300926
nryberg <nryberg@gmail.com>
2010-06-21 22:50:07 -0500

    Working on getting the hang of haml again.

--- config/application.rb
+++ config/application.rb
@@ -38,11 +38,11 @@ module Allinwithus
     # config.i18n.default_locale = :de
 
     # Configure generators values. Many other options are available, be sure to check the documentation.
-config.generators do |g|
-    g.orm             :mongomapper
-    g.template_engine :haml
-    g.test_framework  :test_unit, :fixture => true
-end
+		config.generators do |g|
+				g.orm             :mongomapper
+				g.template_engine :haml
+				g.test_framework  :test_unit, :fixture => true
+		end
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
